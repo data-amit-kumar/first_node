c95dd09e324d2f490a54038321573cc02062d0f0
data-amit-kumar <data2.amitkumar@gmail.com>
2023-12-27 02:25:07 +0530

    change3

--- first.js
+++ first.js
@@ -3,3 +3,20 @@ const multiply =(a,b)=>{
     return(a*b);
 }
 console.log(multiply(10,2));
+
+
+const student={
+    name: "Amit kumar",
+    age: 24,
+    //Below arrow function will get undefined value of this.name and age because here this refers to the global node runtime scope and not to the object.
+    // call: ()=>{
+    //     console.log("Hi my name is "+this.name+" and my age is "+this.age);
+    // }
+    call: function(){
+        console.log("Hi my name is "+this.name+" and my age is "+this.age);
+    }
+    call(){
+        console.log("Hi my name is "+this.name+" and my age is "+this.age);
+    }
+}
+student.call();
